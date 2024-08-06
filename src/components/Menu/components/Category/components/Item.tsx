27036e802e3b3f6ba0bed86995f8a7e4861b8145
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2024-08-06 12:48:41 +0600

    Add mq (xl)

--- src/components/Menu/components/Category/components/Item.tsx
+++ src/components/Menu/components/Category/components/Item.tsx
@@ -36,12 +36,15 @@ export default function ({ item, categoryItemsLength }: ItemProps) {
     data.setTotal(data.total + item.price);
   }
   let liClassName =
-    'border-b border-lightpatty gap-x-4 grid grid-cols-[250px_1fr] items-center list-none p-4';
+    'border-b border-lightpatty gap-x-4 grid grid-cols-[200px_1fr] xl:grid-cols-[250px_1fr] items-center list-none p-4';
   if (categoryItemsLength % 2 !== 0) {
     liClassName += ' last:border-none';
   }
   const figureClassName = item.category === 'Shakes' ? 'shakes' : '';
-  const imgClassName = item.category === 'Shakes' ? 'block h-[250px] rounded' : 'block h-[150px]';
+  const imgClassName =
+    item.category === 'Shakes'
+      ? 'block h-[200px] xl:h-[250px] rounded'
+      : 'block h-[120px] xl:block h-[150px]';
   return (
     <li className={liClassName}>
       <figure className={figureClassName}>
@@ -50,9 +53,9 @@ export default function ({ item, categoryItemsLength }: ItemProps) {
       </figure>
       <dl>
         <dt className='font-bold text-xl'>{item.name}</dt>
-        <dd className='py-2'>{item.description}</dd>
-        <dd className='font-bold py-2'>BDT {item.price}</dd>
-        <dd className='pt-4'>
+        <dd className='py-1 xl:py-2'>{item.description}</dd>
+        <dd className='font-bold py-1 xl:py-2'>BDT {item.price}</dd>
+        <dd className='pt-2 xl:pt-4'>
           <button
             onClick={addOrderItem}
             className='bg-patty border-none font-bold hover:scale-105 hover:shadow hover:shadow-patty px-4 py-2 rounded text-bun w-fit'>
